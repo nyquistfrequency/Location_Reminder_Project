ee4c396cc91637b8ce88943830a88160b1d29f53
Adam <Adam.Natalli@bmw.de>
2023-08-21 15:39:54 +0200

    Set-Up a View Changes to prepare for geofencing implementation

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -44,4 +44,5 @@ class MyApp : Application() {
 			modules(listOf(myModule))
 		}
 	}
+
 }
--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,5 +1,10 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -7,20 +12,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.LocationServices
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
+// import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.ACTION_GEOFENCE_EVENT
+import com.udacity.project4.locationreminders.geofence.createChannel
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
+@SuppressLint("UnspecifiedImmutableFlag")
 class SaveReminderFragment : BaseFragment() {
+
+
 
     // Get the view model this time as a single to be shared with the another fragment
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSaveReminderBinding
+    private lateinit var geofencingClient: GeofencingClient
     private val TAG = SaveReminderFragment::class.java.simpleName
+    private val runningQOrLater = Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.Q
+
+    // A PendingIntent for the Broadcast Receiver that handles geofence transitions.
+//    private val geofencePendingIntent: PendingIntent by lazy {
+//        val intent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+//        intent.action = ACTION_GEOFENCE_EVENT
+//        PendingIntent.getBroadcast(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -31,6 +56,11 @@ class SaveReminderFragment : BaseFragment() {
         setDisplayHomeAsUpEnabled(true)
         binding.viewModel = _viewModel
         return binding.root
+
+//        geofencingClient = LocationServices.getGeofencingClient(requireActivity())
+
+        // Create channel for notifications
+        // createChannel(requireContext())
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -53,12 +83,27 @@ class SaveReminderFragment : BaseFragment() {
             val latitude = _viewModel.latitude
             val longitude = _viewModel.longitude.value
 
+//            _viewModel.validateAndSaveReminder(
+//                ReminderDataItem(title, description,
+//                location,
+//                latitude, longitude)
+//            )
+
 
 
             // TODO: use the user entered reminder details to:
             //  1) add a geofencing request
             //  2) save the reminder to the local db
         }
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        checkPermissionsAndStartGeofencing()
+//    }
+
+    private fun checkPermissionsAndStartGeofencing() {
+        TODO("Not yet implemented")
     }
 
     override fun onDestroy() {
@@ -70,37 +115,37 @@ class SaveReminderFragment : BaseFragment() {
     /*
     *  Requests ACCESS_FINE_LOCATION and (on Android 10+ (Q) ACCESS_BACKGROUND_LOCATION.
     */
-//    @RequiresApi(Build.VERSION_CODES.Q)
-//    private fun requestFineAndBackgroundLocationPermissions() {
-//        if (fineAndBackgroundLocationPermissionsApproved())
-//            return
-//        var permissionsArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-//        val requestCode = when {
-//            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
-//                permissionsArray += Manifest.permission.ACCESS_BACKGROUND_LOCATION
-//                34
-//            }
-//            else -> 33
-//        }
-//
-//        requestPermissions(permissionsArray, requestCode)
-//    }
-//
-//
-//    @RequiresApi(Build.VERSION_CODES.Q)
-//    private fun fineAndBackgroundLocationPermissionsApproved(): Boolean {
-//        val foregroundLocationApproved = (
-//                PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-//                    requireContext(), Manifest.permission.ACCESS_FINE_LOCATION))
-//
-//        val backgroundPermissionApproved =
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-//                    requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-//            } else {
-//                true
-//            }
-//
-//        return foregroundLocationApproved && backgroundPermissionApproved
-//    }
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun requestFineAndBackgroundLocationPermissions() {
+        if (fineAndBackgroundLocationPermissionsApproved())
+            return
+        var permissionsArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+        val requestCode = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+                permissionsArray += Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                34
+            }
+            else -> 33
+        }
+
+        requestPermissions(permissionsArray, requestCode)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun fineAndBackgroundLocationPermissionsApproved(): Boolean {
+        val foregroundLocationApproved = (
+                PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.ACCESS_FINE_LOCATION))
+
+        val backgroundPermissionApproved =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            } else {
+                true
+            }
+
+        return foregroundLocationApproved && backgroundPermissionApproved
+    }
 }
