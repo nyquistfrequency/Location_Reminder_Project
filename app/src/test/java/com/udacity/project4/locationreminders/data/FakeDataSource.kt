eafaf41eef3381b149f68fb4a1e8fe1cc92b665c
Adam <adam.natalli@bmw.de>
2023-08-24 10:47:55 +0200

    Issues with testing...

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,16 +4,16 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var tasks: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var returnError = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         TODO("Return the reminders")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        tasks?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
@@ -21,7 +21,11 @@ class FakeDataSource : ReminderDataSource {
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        tasks?.clear()
+    }
+
+    fun shouldReturnError (boolean: Boolean) {
+        returnError = boolean
     }
 
 
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,57 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.koin.dsl.koinApplication
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
+    //Subject under test
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    //fake data source to be injected in the viewModel
+    private lateinit var fakeReminderDataSource: FakeDataSource
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setupRemindersListViewModel(){
+        fakeReminderDataSource = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeReminderDataSource)
+    }
+
+    @Test
+    fun testInvalidDataSource_returnError(){
+        // Make the FakeDataSource return errors
+        fakeReminderDataSource.shouldReturnError(true)
+
+        // When the DataSource has no valid value
+        fakeReminderDataSource = FakeDataSource(null)
+
+        // Then return an error message
+        assertThat(remindersListViewModel.showSnackBar.getOrAwaitValue(), `is`("Error getting reminders"))
+
+    }
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
 }
